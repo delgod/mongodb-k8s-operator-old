ed05c571462cd787b196c2fb22044de121fd0567
Balbir Thomas <balbir.thomas@canonical.com>
2022-02-14 13:37:58 +0000

    Remove use of stored state
    The MongoDB charm was using stored state to track if the MongoDB
    replica set has been initialized. Use of stored state here is
    not strictly necessary. Hence this commit migrates this logic
    from using a stored state to instead using peer application
    relation data.

--- src/charm.py
+++ src/charm.py
@@ -4,7 +4,6 @@ import logging
 import secrets
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.pebble import PathError, ProtocolError
 
 from ops.main import main
@@ -33,12 +32,9 @@ class MongoDBCharm(CharmBase):
     - Reconfigure replica set anytime number of MongoDB units changes
     - Provides a database relation for any MongoDB client
     """
-    _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-
-        self._stored.set_default(mongodb_initialized=False)
 
         self.port = MONGODB_PORT
 
@@ -49,6 +45,8 @@ class MongoDBCharm(CharmBase):
         self.framework.observe(self.on.stop, self._on_stop)
         self.framework.observe(self.on.update_status, self._on_update_status)
 
+        self.framework.observe(self.on[PEER].relation_created,
+                               self._initialize_peers)
         self.framework.observe(self.on[PEER].relation_changed,
                                self._reconfigure)
         self.framework.observe(self.on[PEER].relation_departed,
@@ -57,7 +55,7 @@ class MongoDBCharm(CharmBase):
         self.framework.observe(self.on.leader_elected,
                                self._on_leader_elected)
 
-        if self._stored.mongodb_initialized and self.mongo.version:
+        if self.mongodb_initialized and self.mongo.version:
             self.mongo_provider = MongoProvider(self, 'database')
         else:
             logger.debug("Mongo Provider not yet Available")
@@ -74,6 +72,11 @@ class MongoDBCharm(CharmBase):
         different from the current specification.
         """
         logger.debug("Running config changed handler")
+
+        if not self.peers:
+            logger.debug("Peer relation does not exist, not starting mongodb")
+            return
+
         container = self.unit.get_container("mongodb")
 
         if not container.can_connect():
@@ -119,7 +122,7 @@ class MongoDBCharm(CharmBase):
             event.defer()
             return
 
-        if not self._stored.mongodb_initialized:
+        if not self.mongodb_initialized:
             self.unit.status = WaitingStatus("Initializing MongoDB")
             try:
                 logger.debug(
@@ -127,7 +130,7 @@ class MongoDBCharm(CharmBase):
                     self.mongo.cluster_hosts
                 )
                 self.mongo.initialize_replica_set(self.mongo.cluster_hosts)
-                self._stored.mongodb_initialized = True
+                self.peers.data[self.app]["mongodb_initialized"] = json.dumps(True)
                 self.peers.data[self.app][
                     "replica_set_hosts"] = json.dumps(self.mongo.cluster_hosts)
 
@@ -172,7 +175,7 @@ class MongoDBCharm(CharmBase):
                 self.restart()
             return
 
-        if not self._stored.mongodb_initialized:
+        if not self.mongodb_initialized:
             status_message = "mongodb not initialized"
             self.unit.status = WaitingStatus(status_message)
             return
@@ -226,6 +229,14 @@ class MongoDBCharm(CharmBase):
         security_key = data.get('security_key', None)
         if security_key is None:
             self.peers.data[self.app]['security_key'] = str(self.security_key)
+
+    def _initialize_peers(self, event):
+        """Initialize peer relation data.
+        """
+        if not self.unit.is_leader():
+            return
+
+        event.relation.data[self.app]["mongodb_initialized"] = json.dumps(False)
 
     ##############################################
     #               PROPERTIES                   #
@@ -312,6 +323,21 @@ class MongoDBCharm(CharmBase):
         """
         return self.model.get_relation(PEER)
 
+    @property
+    def mongodb_initialized(self):
+        if not self.peers:
+            return False
+
+        try:
+            peer_data = self.peers.data[self.app]
+        except AttributeError:
+            return False
+
+        has_intialized = json.loads(peer_data.get("mongodb_initialized", "false"))
+
+        return has_intialized
+
+    @property
     def replica_set_hosts(self):
         """Fetch current list of hosts in the replica set.
 
@@ -367,4 +393,4 @@ class MongoDBCharm(CharmBase):
 
 
 if __name__ == "__main__":
-    main(MongoDBCharm, use_juju_for_storage=True)
+    main(MongoDBCharm)
